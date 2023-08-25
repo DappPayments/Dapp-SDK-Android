bf95f1609a64fdcf953572cd8f3716fcefa049b6
Jorge Alfaro <jorge@dapp.mx>
2023-08-24 23:55:42 -0600

    correcciones cashin dapp

--- vendor/src/main/java/mx/dapp/sdk/vendor/dto/DappCashIn.java
+++ vendor/src/main/java/mx/dapp/sdk/vendor/dto/DappCashIn.java
@@ -51,34 +51,6 @@ public class DappCashIn implements Parcelable {
         }
     }
 
-
-    protected DappCashIn(Parcel in) {
-        id = in.readString();
-        reference = in.readString();
-        merchantReference = in.readString();
-        authNumber = in.readString();
-        if (in.readByte() == 0) {
-            amount = null;
-        } else {
-            amount = in.readDouble();
-        }
-        if (in.readByte() == 0) {
-            fee = null;
-        } else {
-            fee = in.readDouble();
-        }
-        if (in.readByte() == 0) {
-            total = null;
-        } else {
-            total = in.readDouble();
-        }
-        currency = in.readString();
-        byte tmpRefunded = in.readByte();
-        refunded = tmpRefunded == 0 ? null : tmpRefunded == 1;
-        walletId = in.readString();
-        walletName = in.readString();
-    }
-
     public String getId() {
         return id;
     }
@@ -127,53 +99,6 @@ public class DappCashIn implements Parcelable {
         return walletName;
     }
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(id);
-        dest.writeString(reference);
-        dest.writeString(merchantReference);
-        dest.writeString(authNumber);
-        if (amount == null) {
-            dest.writeByte((byte) 0);
-        } else {
-            dest.writeByte((byte) 1);
-            dest.writeDouble(amount);
-        }
-        if (fee == null) {
-            dest.writeByte((byte) 0);
-        } else {
-            dest.writeByte((byte) 1);
-            dest.writeDouble(fee);
-        }
-        if (total == null) {
-            dest.writeByte((byte) 0);
-        } else {
-            dest.writeByte((byte) 1);
-            dest.writeDouble(total);
-        }
-        dest.writeString(currency);
-        dest.writeByte((byte) (refunded == null ? 0 : refunded ? 1 : 2));
-        dest.writeString(walletId);
-        dest.writeString(walletName);
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    public static final Creator<DappCashIn> CREATOR = new Creator<DappCashIn>() {
-        @Override
-        public DappCashIn createFromParcel(Parcel in) {
-            return new DappCashIn(in);
-        }
-
-        @Override
-        public DappCashIn[] newArray(int size) {
-            return new DappCashIn[size];
-        }
-    };
-
     private Date parseDateString(String strDate) throws ParseException {
         SimpleDateFormat sdf;
         Locale locale = Locale.US;
@@ -184,4 +109,70 @@ public class DappCashIn implements Parcelable {
         }
         return sdf.parse(strDate);
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id);
+        dest.writeString(this.reference);
+        dest.writeString(this.merchantReference);
+        dest.writeString(this.authNumber);
+        dest.writeValue(this.amount);
+        dest.writeValue(this.fee);
+        dest.writeValue(this.total);
+        dest.writeString(this.currency);
+        dest.writeValue(this.refunded);
+        dest.writeLong(this.date != null ? this.date.getTime() : -1);
+        dest.writeString(this.walletId);
+        dest.writeString(this.walletName);
+    }
+
+    public void readFromParcel(Parcel source) {
+        this.id = source.readString();
+        this.reference = source.readString();
+        this.merchantReference = source.readString();
+        this.authNumber = source.readString();
+        this.amount = (Double) source.readValue(Double.class.getClassLoader());
+        this.fee = (Double) source.readValue(Double.class.getClassLoader());
+        this.total = (Double) source.readValue(Double.class.getClassLoader());
+        this.currency = source.readString();
+        this.refunded = (Boolean) source.readValue(Boolean.class.getClassLoader());
+        long tmpDate = source.readLong();
+        this.date = tmpDate == -1 ? null : new Date(tmpDate);
+        this.walletId = source.readString();
+        this.walletName = source.readString();
+    }
+
+    protected DappCashIn(Parcel in) {
+        this.id = in.readString();
+        this.reference = in.readString();
+        this.merchantReference = in.readString();
+        this.authNumber = in.readString();
+        this.amount = (Double) in.readValue(Double.class.getClassLoader());
+        this.fee = (Double) in.readValue(Double.class.getClassLoader());
+        this.total = (Double) in.readValue(Double.class.getClassLoader());
+        this.currency = in.readString();
+        this.refunded = (Boolean) in.readValue(Boolean.class.getClassLoader());
+        long tmpDate = in.readLong();
+        this.date = tmpDate == -1 ? null : new Date(tmpDate);
+        this.walletId = in.readString();
+        this.walletName = in.readString();
+    }
+
+    public static final Creator<DappCashIn> CREATOR = new Creator<DappCashIn>() {
+        @Override
+        public DappCashIn createFromParcel(Parcel source) {
+            return new DappCashIn(source);
+        }
+
+        @Override
+        public DappCashIn[] newArray(int size) {
+            return new DappCashIn[size];
+        }
+    };
 }
